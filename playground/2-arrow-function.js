0f47225e4beb073101d197a1dac4bd0ceca93df0
Lohitaksh Singla <lohitakshsingla0@gmail.com>
2022-06-23 03:44:04 +0200

    Update 2-arrow-function.js

--- playground/2-arrow-function.js
+++ playground/2-arrow-function.js
@@ -6,6 +6,20 @@
 //     return x*x
 // }
 
-const square = (x) => x*x
+// const square = (x) => x*x
 
-console.log(square(3))
+// console.log(square(3))
+
+const event = {
+    name  : 'Birthday party',
+    guestList : ['Lohit', 'Mustan','Ashish'],
+    printGuestList(){
+        console.log('Guest List for ' + this.name)
+        this.guestList.forEach(guest => {
+            console.log(guest + ' is attending ' + this.name)
+            
+        })
+    }
+}
+
+event.printGuestList()
